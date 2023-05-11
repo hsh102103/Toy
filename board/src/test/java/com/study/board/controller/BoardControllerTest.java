d0def10c3473d603a3dda84c216a387935edfe81
hsh102103 <hsh102103@naver.com>
2023-05-11 23:53:05 +0900

    [#3]Test: getBoardById
    게시글 단일 조회 테스트

--- board/src/test/java/com/study/board/controller/BoardControllerTest.java
+++ board/src/test/java/com/study/board/controller/BoardControllerTest.java
@@ -1,6 +1,7 @@
 package com.study.board.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -11,6 +12,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import java.util.HashMap;
 import java.util.Map;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
