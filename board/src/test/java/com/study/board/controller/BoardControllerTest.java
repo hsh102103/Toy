8be702a9556f4e8099fd845ed35e166760d60e54
hsh102103 <hsh102103@naver.com>
2023-05-11 23:54:26 +0900

    [#3]add: createBoard displayName

--- board/src/test/java/com/study/board/controller/BoardControllerTest.java
+++ board/src/test/java/com/study/board/controller/BoardControllerTest.java
@@ -28,6 +28,7 @@ class BoardControllerTest {
     private MockMvc mvc;
 
     @Test
+    @DisplayName("게시글 생성 성공 - web layer")
     void createBoard() throws Exception {
         // given
         Map<String, String> info = new HashMap<>();
@@ -38,8 +39,20 @@ class BoardControllerTest {
         mvc.perform(post("/api/board")
                         .contentType(MediaType.APPLICATION_JSON) // 요청에 담긴 데이터의 미디어타입
                         .accept(MediaType.APPLICATION_JSON) // 클라이언트가 처리 가능한 미디어타입
-                        .content(objectMapper.writeValueAsString(info)))
-                .andExpect(status().isCreated())
+                        .content(objectMapper.writeValueAsString(info))
+                ).andExpect(status().isCreated())
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("게시글 조회 성공 - web layer")
+    void getBoardById() throws Exception {
+        // 반환하는 객체의 일부만 확인할 수는 없나..
+
+        Long id = 6L;
+        mvc.perform(get("/api/board/" + id)
+                        .contentType(MediaType.APPLICATION_JSON)
+                ).andExpect(status().isOk())
                 .andDo(print());
     }
 }
