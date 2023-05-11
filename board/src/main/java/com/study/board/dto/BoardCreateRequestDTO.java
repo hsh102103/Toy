199a5bb66926000c72effde9e6aedd4a109175a7
hsh102103 <hsh102103@naver.com>
2023-05-11 23:56:15 +0900

    [#2]Modify: BoardRequestDTO name
    DTO 클래스 이름 수정

--- board/src/main/java/com/study/board/dto/BoardCreateRequestDTO.java
+++ board/src/main/java/com/study/board/dto/BoardCreateRequestDTO.java
@@ -5,7 +5,7 @@ import lombok.Setter;
 
 @Getter
 @Setter
-public class BoardRequestDTO {
+public class BoardCreateRequestDTO {
     private Long id;
     private String title;
     private String content;
